5ee0b4f0926502aa18ce776d5ff00afe6527b379
chinapostm <chinapostm@gmail.com>
2022-03-10 19:47:36 +0800

    常规修复

--- config/database.php
+++ config/database.php
@@ -6,15 +6,15 @@ return [
     // 数据库主机
     'host'      => 'localhost',
     // 数据库名称
-    'database'  => '',
+    'database'  => 'demo',
     // 数据库账号
-    'username'  => '',
+    'username'  => 'demo',
     // 数据库密码
-    'password'  => '',
+    'password'  => 'demo',
     // 数据库字符集
     'charset'   => 'utf8mb4',
     // 数据库排序
     'collation' => 'utf8mb4_general_ci',
     // 数据库前缀
-    'prefix'    => '',
+    'prefix'    => 'sm_',
 ];
